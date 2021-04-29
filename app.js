14497c4f8f0163507c9eb1b6b6e9ec0fd4c1e04a
John Lee <lee.john.tk@gmail.com>
2021-04-29 14:16:06 -0500

    started wikistack pair programming project

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
