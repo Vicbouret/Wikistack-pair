4b22c28fd671b877cc5feb7b99dcc444d7393c73
Vicbouret <victorhbouret@hotmail.com>
2021-04-29 16:29:15 -0500

    Added sequelize and synced Databases

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
+const html = require("./views/layout");
+const { db, Page, User } = require("./models");
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: false }));
-app.use(morgan('dev'));
-app.use(express.static('./public'));
+app.use(morgan("dev"));
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+db.authenticate().then(() => {
+  console.log("connected to the database. Hooray!");
+});
+
+async function dbSync() {
+  // await Page.sync({ force: true });
+  // await User.sync({ force: true });
+  await db.sync();
+}
+
+app.get("/", (req, res) => {
+  res.send(html());
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+dbSync();
